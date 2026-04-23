bffc32fb23bff14ee3d2314637d9aa65d195fa40
sduranfo <131090518+sduranfo@users.noreply.github.com>
2026-04-23 22:12:35 +0000

    feat: actualizo mensaje de bienvenida

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ function iniciarServidor() {
 
   const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hola Mundo DevOS\n');
+    res.end('Hola DevOS\n');
   });
 
   server.listen(PORT, () => {
